77161001e0ec7fd95760ace50399f671ae9618c2
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-03-12 10:33:12 +0700

    menambahkan jumalah data

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int arr(20);
-int n;
+int arr(20);      //membuat array dengan panjang data 20
+int n;            //membuat variable inputan n
+
+void input (){
+    while (true){
+        cout << "Masukkan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArray Tidak Boleh Lebih dari 20\n";
+        }
+    }
+    cout << endl;
+    cout << "================================";
+    cout << "=====Masukkan Element Array=====";
+    cout << "================================";
+
+    for (int i = 0; i < n; i++ ){
+        cout << "Data ke-" << (i+1) << ": ";
+        cin >> arr[i];
+    }
+}
